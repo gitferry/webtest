b46fd272a4d77c5de17bcdf89c66034b9fc6ffa0
gaigai <greferry@gmail.com>
2014-09-25 11:09:14 +0800

    add port detail

--- peni/models.py
+++ peni/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class PortTarget(models.Model):
 	sys_info = models.CharField(max_length=20)
+	ip = models.CharField(max_length=20)
 
 
 class Port(models.Model):
--- peni/views.py
+++ peni/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from func import information_gathering, result
+from models import PortTarget, Port
 
 def home(request):
 	return render(request, 'peni/home.html', {})
 
 def search(request):
 	search_url = request.GET['q']
+	r = result.result()
+	information_gathering.nmap(search_url, r)
+	target = PortTarget()
+	target.sys_info = r.operatingsystem
+	target.ip = search_url
+	for key in r.portservice:
+		port = Port()
+		port.target = target.pk
+		port.number = key
+		port.details = r.portservice[key]
+	return render(request, 'peni/portdetail.html', {'target': target})
