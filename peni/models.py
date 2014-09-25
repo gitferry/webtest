58181649105680000d76966a38225092a562259c
gaigai <greferry@gmail.com>
2014-09-25 10:32:59 +0800

    add model

--- peni/models.py
+++ peni/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class PortTarget(models.Model):
+	sys_info = models.CharField(max_length=20)
+
+
+class Port(models.Model):
+	target = models.ForeignKey(PortTarget)
+	number = models.IntegerField()
+	details = models.CharField(max_length=100)
